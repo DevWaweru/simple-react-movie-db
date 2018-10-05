3bb1843135946a36eab43320414cf5bc624f8488
Richard Waweru <developer.waweru@gmail.com>
2018-10-06 01:47:19 +0300

    - Add react router module and query string - Add customer, movie form, rentals and not found components - Implement the react router module to facilitate routing to these components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import FetchMovies from './components/movies';
+import Customers from './components/customer';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main className='container'>
+      <Switch>
+        <Route path="/movies" component={FetchMovies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from='/' exact to='/movies'/>
+        <Redirect to='/not-found'/>
+        </Switch>
+      </main>
     );
   }
 }
