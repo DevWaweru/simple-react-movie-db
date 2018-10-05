aaab89422a44bcfa89480d0f71d25c119a4a4358
Richard Waweru <developer.waweru@gmail.com>
2018-10-06 02:05:27 +0300

    - Add navbar for easy navigation between created routes - Add margin below the navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import './App.css';
 import FetchMovies from './components/movies';
 import Customers from './components/customer';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <main className='container'>
-      <Switch>
-        <Route path="/movies" component={FetchMovies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from='/' exact to='/movies'/>
-        <Redirect to='/not-found'/>
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar/>
+        <main className='container'>
+          <Switch>
+            <Route path="/movies" component={FetchMovies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from='/' exact to='/movies'/>
+            <Redirect to='/not-found'/>
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
